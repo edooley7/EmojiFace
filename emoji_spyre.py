d288e28ef009eb01e7aa835a38720f9aed724cc4
Erin <edooley7@gmail.com>
2016-01-17 21:20:03 -0500

    spanish

--- emoji_spyre.py
+++ emoji_spyre.py
@@ -163,4 +163,4 @@ class EmojiApp(server.App):
 
 if __name__ == '__main__':
     app = EmojiApp()
-    app.launch(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.launch(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug = True)
